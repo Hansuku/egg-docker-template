5ebdf4c347774310568727c55675427eeecffc36
hansuku <1556207795@qq.com>
2019-11-25 14:03:35 +0800

    clear

--- node/config.js
+++ node/config.js
@@ -1,5 +1,2 @@
-const AppID = "5d5e6583";
-const TTSAppKey = "92be4897ea42803ff1c21c589db0e783";
-const qiniuAccessKey = 'Tt4ODzPfo6_nHL5wEfrPnyCV1dkDzOW6SYXnAJea';
-const qiniuSecretKey = 'njCF2LNUHeNWL3vw7UHV5sBG8Yq03kA-d61-w7aX';
-module.exports = { AppID, TTSAppKey, qiniuAccessKey, qiniuSecretKey }
+const Test = "test";
+module.exports = { Test }
